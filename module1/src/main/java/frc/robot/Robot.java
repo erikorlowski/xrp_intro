8f03af5f3e12f5876c7aae267f04243751c9da98
erikorlowski <erik.orlowski@gmail.com>
2025-10-17 22:03:52 -0500

    Module 1

---
+++ module1/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.xrp.XRPMotor;
+import edu.wpi.first.wpilibj.xrp.XRPServo;
+
+public class Robot extends TimedRobot {
+  // Define Objects here!
+  XRPMotor leftMotor = new XRPMotor(0);
+  
+
+
+
+  public Robot() {}
+
+  public void teleopPeriodic() {
+    // Call Methods here!
+
+
+
+
+
+  }
+
+}
